291114c96020f53d1cb9c8305b2ca760651b2052
samahakal <91861509+samahakal@users.noreply.github.com>
2022-09-02 11:48:30 +0530

    Create September_1.cpp
     Count Good Nodes in Binary Tree

---
+++ September_1.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+//  Count Good Nodes in Binary Tree
+class Solution {
+public:
+    
+    int cnt = 0;
+    void dfs(TreeNode *root,int mx){
+        if(root == NULL){
+            return;
+        }
+        
+        if(root->val >= mx){
+            cnt++;
+            mx = max(mx,root->val);
+        }
+        dfs(root->left,mx);
+        dfs(root->right,mx);
+    }
+    
+    int goodNodes(TreeNode* root) {
+        dfs(root,root->val);
+        return cnt;
+    }
+};
