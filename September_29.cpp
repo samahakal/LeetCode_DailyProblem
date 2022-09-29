920bb28eeb92f81cdb9031b7c901a3a3957ca4ba
samahakal <91861509+samahakal@users.noreply.github.com>
2022-09-29 23:27:21 +0530

    658. Find K Closest Elements

---
+++ September_29.cpp
@@ -0,0 +1,23 @@
+// 658. Find K Closest Elements
+// https://leetcode.com/problems/find-k-closest-elements/
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        int left = 0;
+        int right = n-k;
+        
+        // we want starting point -> place left to it's correct index
+        while(left<right){
+            int mid = left + (right - left)/2;
+            if(x - arr[mid] <= arr[mid+k] - x){
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return vector<int>(arr.begin()+left,arr.begin()+left+k);
+    }
+};
